11f1a5106d06d2c9e20c8a098498f909351397dc
Himanshu <himanshu88140@gmail.com>
2025-02-17 21:44:00 +0530

    update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <footer id='#contact' className={styles.container}>
     <div className={styles.text}>
         <h2>Contact me</h2>
-        <p>Feel free to reach out!</p>
+        <p>Feel free to reach out!</p> 
     </div>
     <ul className={styles.links}>
         <li className={styles.link}>
