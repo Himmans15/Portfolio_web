4bc7aeb33bfc6e77403f2a48013f877ad9b45ade
Himmans Bhhardwaj <himansbhardwaj1566@gmail.com>
2025-02-17 21:47:30 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './About.module.css';
 import { getImageUrl } from "../../utils";
-
+ 
 const About = () => {
   return (
     <section className={styles.container} id='about'>
